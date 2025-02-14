6434569fca3aef3f0fe972c55387412584efcb42
= <=>
2025-02-14 20:59:10 +0400

    Geolocation, error handling

--- mobileModule02/medium_weather_app/app/index.tsx
+++ mobileModule02/medium_weather_app/app/index.tsx
@@ -1,4 +1,3 @@
-// src/screens/Index.tsx
 import { View, Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import AppBar from "@/components/AppBar";
@@ -10,20 +9,35 @@ import { useState, useRef } from "react";
 import { useLocation } from "@/hooks/useLocation";
 import { changePage, setCityName } from "@/hooks/utils";
 
-type locationProps = {
-  lat: number;
-};
-
-export default function Index({ lat }: locationProps) {
+export default function Index() {
   const [pageIndex, setPageIndex] = useState(0);
   const [name, setName] = useState("");
   const pagerRef = useRef<PagerView>(null);
 
   const { location, error } = useLocation();
-
+  const showError = error && (!name || name == "");
   return (
     <SafeAreaProvider>
       <AppBar setCityName={(name: string) => setCityName(name, setName)} />
+      
+      {}
+      {showError && (
+        <View
+          style={{
+            position: "absolute",
+            top: 130,
+            width: "100%",
+            backgroundColor: "red",
+            padding: 10,
+            zIndex: 1000,
+          }}
+        >
+          <Text style={{ fontSize: 20, color: "white" }}>
+            Error: {error}
+          </Text>
+        </View>
+      )}
+
       <PagerView
         ref={pagerRef}
         style={{ flex: 1 }}
@@ -32,18 +46,18 @@ export default function Index({ lat }: locationProps) {
         <View key="1" style={{ justifyContent: "center", alignItems: "center" }}>
           <Text style={{ fontSize: 50 }}>{name || ""}</Text>
           <Text style={{ fontSize: 50 }}>Currently</Text>
-          <Text style={{ fontSize: 20 }}>
-            Latitude: {location ? location.lat : error ? error : "Loading..."}
-          </Text>
-          <Text style={{ fontSize: 20 }}>
-            Longitude: {location ? location.lon : error ? error : "Loading..."}
-          </Text>
+          {location ? (
+            <>
+              <Text style={{ fontSize: 20 }}>Latitude: {location.lat}</Text>
+              <Text style={{ fontSize: 20 }}>Longitude: {location.lon}</Text>
+            </>
+          ) : null}
         </View>
         <View key="2" style={{ justifyContent: "center", alignItems: "center" }}>
-          <Today cityName={lat} />
+          <Today cityName={name} lat={location?.lat} log={location?.lon} />
         </View>
         <View key="3" style={{ justifyContent: "center", alignItems: "center" }}>
-          <Weekly cityName={name || ""} />
+          <Weekly cityName={name} lat={location?.lat} log={location?.lon} />
         </View>
       </PagerView>
       <BottomBar onTabPress={(index) => changePage(index, pagerRef, setPageIndex)} />
--- mobileModule02/medium_weather_app/app/weekly.tsx
+++ mobileModule02/medium_weather_app/app/weekly.tsx
@@ -1,15 +1,19 @@
 import { View, Text } from "react-native";
 
 type pageProps = {
-    cityName?: string
+    cityName?: string,
+    lat?: number,
+    log?: number
 }
 
-export default function Weekly({cityName}: pageProps)
+export default function Weekly({cityName, lat, log}: pageProps)
 {
     return (
         <View style={{justifyContent: "center", alignItems: "center"}}>
             <Text style={{fontSize: 50}}>{cityName || ""}</Text>
             <Text style={{fontSize: 50}}>Weekly</Text>
+            <Text style={{ fontSize: 20 }}>Latitude: {lat}</Text>
+            <Text style={{ fontSize: 20 }}>Longitude: {log}</Text>
         </View>
     );
 }
