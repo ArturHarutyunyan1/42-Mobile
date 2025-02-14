06e992fcd34d3242915aab347c7faee52daed360
= <=>
2025-02-14 15:27:42 +0400

    Swipable router

--- mobileModule01/weather_app/app/index.tsx
+++ mobileModule01/weather_app/app/index.tsx
@@ -1,13 +1,25 @@
-import { ScrollView, Text, View } from "react-native";
-import AppBar from "@/components/AppBar";
+import { View, Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import AppBar from "@/components/AppBar";
+import PagerView from "react-native-pager-view";
+import Today from "./today";
+import Weekly from "./weekly";
 
 export default function Index() {
   return (
     <SafeAreaProvider>
-      <AppBar></AppBar>
-      <ScrollView>
-      </ScrollView>
+      <AppBar />
+      <PagerView style={{ flex: 1 }} initialPage={1}>
+      <View key="3" style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#f5f5f5" }}>
+        <Weekly></Weekly>
+        </View>
+        <View key="1" style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#f5f5f5" }}>
+          <Text style={{ fontSize: 24 }}>Home Page</Text>
+        </View>
+        <View key="2" style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#e0e0e0" }}>
+          <Today></Today>
+        </View>
+      </PagerView>
     </SafeAreaProvider>
   );
 }
---
+++ mobileModule01/weather_app/app/today.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import AppBar from "@/components/AppBar";
+
+export default function Today()
+{
+    return (
+        <View>
+            {/* <AppBar></AppBar> */}
+            <Text>Today Page</Text>
+        </View>
+    );
+}
