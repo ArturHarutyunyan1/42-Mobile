fcf0562adc7baa44dab4a19d0a0999a13d441237
Artur Harutyunyan <arturhar@outlook.com>
2025-02-21 22:30:40 +0400

    Display, value

--- mobileModule00/ex02/ex02/ContentView.swift
+++ mobileModule00/ex02/ex02/ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         GeometryReader { geometry in
-            VStack(spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
                 HStack (alignment: .top) {
                     Text("Calculator")
                         .font(.system(size: 32, weight: .medium))
@@ -11,12 +11,24 @@ struct ContentView: View {
                 }
                 .frame(width: geometry.size.width, height: geometry.size.height * 0.1)
                 .background(.blue)
-                Spacer()
-                VStack {
-                    
+                VStack () {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Text("0")
+                            .font(.system(size: 52, weight: .medium))
+                            .padding(10)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(20)
                 }
+                .frame(width: geometry.size.width, height: geometry.size.height * 0.3)
+                .background(.red)
+                Spacer()
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(.bottom, geometry.safeAreaInsets.bottom)
+            .ignoresSafeArea(.keyboard)
         }
     }
 }
