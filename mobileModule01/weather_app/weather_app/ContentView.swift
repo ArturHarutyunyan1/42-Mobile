aeb2f068bc802ee052462d1b78a625cbc52dcc9b
Artur Harutyunyan <arturhar@outlook.com>
2025-03-01 14:31:46 +0400

    Weather app start

---
+++ mobileModule01/weather_app/weather_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather_app
+//
+//  Created by Artur Harutyunyan on 01.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
