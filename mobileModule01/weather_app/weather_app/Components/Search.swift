4404279a075357cd4f77457958d540ba2581dc88
Artur Harutyunyan <arturhar@outlook.com>
2025-03-02 14:59:11 +0400

    App bar, fixed gesture

---
+++ mobileModule01/weather_app/weather_app/Components/Search.swift
@@ -0,0 +1,18 @@
+//
+//  Search.swift
+//  weather_app
+//
+//  Created by Artur Harutyunyan on 02.03.25.
+//
+
+import SwiftUI
+
+struct Search: View {
+    var body: some View {
+        HStack {
+            Text("App bar")
+        }
+        .frame(width: UIScreen.main.bounds.width, height: 60)
+        .background(Color.red)
+    }
+}
--- mobileModule01/weather_app/weather_app/ContentView.swift
+++ mobileModule01/weather_app/weather_app/ContentView.swift
@@ -1,3 +1,11 @@
+//
+//  ContentView.swift
+//  weather_app
+//
+//  Created by Artur Harutyunyan on 01.03.25.
+//
+
+
 import SwiftUI
 
 enum AppTab: CaseIterable, Hashable {
@@ -8,55 +16,60 @@ struct ContentView: View {
     @State private var selectedTab: AppTab = .currently
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                if selectedTab == .currently {
-                    Home()
-                        .id("currently")
-                        .transition(.asymmetric(
-                            insertion: AnyTransition.move(edge: .trailing).combined(with: .opacity),
-                            removal: AnyTransition.move(edge: .leading).combined(with: .opacity)
-                        ))
-                } else if selectedTab == .today {
-                    Today()
-                        .id("today")
-                        .transition(.asymmetric(
-                            insertion: AnyTransition.move(edge: .trailing).combined(with: .opacity),
-                            removal: AnyTransition.move(edge: .leading).combined(with: .opacity)
-                        ))
-                } else if selectedTab == .weekly {
-                    Weekly()
-                        .id("weekly")
-                        .transition(.asymmetric(
-                            insertion: AnyTransition.move(edge: .trailing).combined(with: .opacity),
-                            removal: AnyTransition.move(edge: .leading).combined(with: .opacity)
-                        ))
-                }
-                VStack {
-                    Spacer()
-                    Navigation(selectedTab: $selectedTab)
-                        .frame(height: 80)
-                        .padding(.horizontal)
-                }
-            }
-            .animation(.smooth(duration: 0.3), value: selectedTab)
-            .navigationBarBackButtonHidden(true)
-        }
-        .gesture(
-            DragGesture(minimumDistance: 50)
-                .onEnded { value in
-                    let tabs = AppTab.allCases
-                    if let currentIndex = tabs.firstIndex(of: selectedTab) {
-                        if value.translation.width > 0 {
-                            let newIndex = (currentIndex - 1 + tabs.count) % tabs.count
-                            selectedTab = tabs[newIndex]
-                        } else {
-                            let newIndex = (currentIndex + 1) % tabs.count
-                            selectedTab = tabs[newIndex]
-                        }
+        VStack (spacing: 0) {
+            Search()
+            NavigationStack {
+                ZStack {
+                    if selectedTab == .currently {
+                        Home()
+                            .id("currently")
+                            .transition(.asymmetric(
+                                insertion: AnyTransition.move(edge: .trailing).combined(with: .opacity),
+                                removal: AnyTransition.move(edge: .leading).combined(with: .opacity)
+                            ))
+                    } else if selectedTab == .today {
+                        Today()
+                            .id("today")
+                            .transition(.asymmetric(
+                                insertion: AnyTransition.move(edge: .trailing).combined(with: .opacity),
+                                removal: AnyTransition.move(edge: .leading).combined(with: .opacity)
+                            ))
+                    } else if selectedTab == .weekly {
+                        Weekly()
+                            .id("weekly")
+                            .transition(.asymmetric(
+                                insertion: AnyTransition.move(edge: .trailing).combined(with: .opacity),
+                                removal: AnyTransition.move(edge: .leading).combined(with: .opacity)
+                            ))
+                    }
+                    VStack {
+                        Spacer()
+                        Navigation(selectedTab: $selectedTab)
+                            .frame(height: 80)
+                            .padding(.horizontal)
                     }
                 }
-        )
+                .animation(.linear(duration: 0.3), value: selectedTab)
+                .navigationBarBackButtonHidden(true)
+            }
+            .gesture(
+                DragGesture(minimumDistance: 50)
+                    .onEnded { value in
+                        if abs(value.translation.width) > abs(value.translation.height) {
+                            let tabs = AppTab.allCases
+                            if let currentIndex = tabs.firstIndex(of: selectedTab) {
+                                if value.translation.width > 0 {
+                                    let newIndex = (currentIndex - 1 + tabs.count) % tabs.count
+                                    selectedTab = tabs[newIndex]
+                                } else {
+                                    let newIndex = (currentIndex + 1) % tabs.count
+                                    selectedTab = tabs[newIndex]
+                                }
+                            }
+                        }
+                    }
+            )
+        }
     }
 }
 
