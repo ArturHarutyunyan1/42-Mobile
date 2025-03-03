ea81f469aa35e65595d24f7cee93bb143e7b474c
Artur Harutyunyan <arturhar@outlook.com>
2025-03-03 17:56:12 +0400

    Start

---
+++ mobileModule02/medium_weather_app/medium_weather_app/medium_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  medium_weather_appApp.swift
+//  medium_weather_app
+//
+//  Created by Artur Harutyunyan on 03.03.25.
+//
+
+import SwiftUI
+
+@main
+struct medium_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
