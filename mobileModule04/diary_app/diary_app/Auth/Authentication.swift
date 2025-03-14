a7fa31d2de1c2b2bdacbf33820002688d12b58d2
Artur Harutyunyan <arturhar@outlook.com>
2025-03-14 21:09:41 +0400

    Details

--- mobileModule04/diary_app/diary_app/Auth/Authentication.swift
+++ mobileModule04/diary_app/diary_app/Auth/Authentication.swift
@@ -17,7 +17,7 @@ enum AuthenticationError : Error {
 @MainActor
 class Authentication : ObservableObject {
     @Published var isLoggedIn = true
-    @Published var userEmail = "arturhar@outlook.com"
+    @Published var userEmail = "arturhar2004@gmail.com"
     
 //    init() {
 //        self.isLoggedIn = (Auth.auth().currentUser != nil)
--- mobileModule04/diary_app/diary_app/Components/NoteDetails.swift
+++ mobileModule04/diary_app/diary_app/Components/NoteDetails.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct NoteDetails: View {
     @Binding var noteDetails: Notes?
+    @EnvironmentObject var dataManager: DataManager
     var body: some View {
-        Text("\(noteDetails?.date ?? "asdadasd")")
+        GeometryReader {geometry in
+            let color = DataManager().stringToColor(noteDetails?.style ?? "")
+            VStack {
+                HStack {
+                    Text("Edit note")
+                        .foregroundStyle(.darkBlue)
+                        .font(.system(size: 20))
+                    Spacer()
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "trash")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                    })
+                    .foregroundStyle(.red)
+                }
+                Text("\(noteDetails?.title ?? "")")
+                    .foregroundStyle(.vernagir)
+                    .font(.system(size: 35))
+                    .fontWeight(.bold)
+                TextEditor(text: Binding(
+                    get: {noteDetails?.text ?? ""},
+                    set: {noteDetails?.text = $0}
+                ))
+                .scrollContentBackground(.hidden)
+                .foregroundStyle(.bovandakutyun)
+                .font(.system(size: 23))
+                Spacer()
+            }
+            .padding()
+            .frame(width: geometry.size.width, height: geometry.size.height)
+            .background(color)
+        }
     }
 }
