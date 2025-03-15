cad7f7c80bf06e7dc996cdc9f8c9b6a3ce3d1e8b
Artur Harutyunyan <arturhar@outlook.com>
2025-03-15 16:21:46 +0400

    Last 2 entries

--- mobileModule05/advanced_diary_app/advanced_diary_app/Views/Profile.swift
+++ mobileModule05/advanced_diary_app/advanced_diary_app/Views/Profile.swift
@@ -11,25 +11,32 @@ import Firebase
 import FirebaseAuth
 import GoogleSignIn
 import FirebaseCore
+import MasonryStack
 
 struct Profile: View {
     @StateObject var authenticationManager: Authentication
     @EnvironmentObject var dataManager: DataManager
     @State private var logoutAlert = false
     @State private var errorAlert = false
+    @State private var showDetails = false
+    @State private var showPopup = false
+    @State private var noteDetails: Notes?
+    var userNotesCount: Int {
+        dataManager.diary.filter { $0.usermail == authenticationManager.userEmail }.count
+    }
     var body: some View {
-        HStack {
-            Spacer()
-            Button (action: {
-                logoutAlert = true
-            }, label: {
-                Image(systemName: "rectangle.portrait.and.arrow.right")
-                    .resizable()
-                    .frame(width: 30, height: 30)
-            })
-            .foregroundStyle(.red)
-        }
         ScrollView {
+            HStack {
+                Spacer()
+                Button (action: {
+                    logoutAlert = true
+                }, label: {
+                    Image(systemName: "rectangle.portrait.and.arrow.right")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                })
+                .foregroundStyle(.red)
+            }
             VStack {
                 HStack {
                     if let url = authenticationManager.userData.avatarURL {
@@ -52,9 +59,82 @@ struct Profile: View {
                         Text(authenticationManager.userData.name)
                             .font(.system(size: 30))
                             .font(.headline)
+                        Spacer()
                     }
                 }
             }
+            VStack {
+                HStack {
+                    Text("Last 2 entries")
+                    Spacer()
+                }
+                .padding()
+                if userNotesCount < 1 {
+                    Text("No recent notes")
+                } else {
+                    VStack {
+                        ForEach(dataManager.diary
+                            .filter { $0.usermail == authenticationManager.userEmail }
+                            .sorted { $0.date > $1.date}
+                            .prefix(2),
+                                id: \.id) { note in
+                            let color = dataManager.stringToColor(note.style)
+                            let mood = note.feeling.components(separatedBy: " ").first ?? "Mood"
+                            let date = note.date.components(separatedBy: " ")
+                            Button(action: {
+                                showDetails = true
+                                noteDetails = note
+                            }, label: {
+                                VStack(alignment: .leading) {
+                                    Text(note.title)
+                                        .font(.headline)
+                                        .foregroundStyle(.vernagir)
+                                    Text(mood)
+                                        .fixedSize(horizontal: false, vertical: true)
+                                        .foregroundStyle(.bovandakutyun)
+                                    Text(note.text)
+                                        .fixedSize(horizontal: false, vertical: true)
+                                        .foregroundStyle(.bovandakutyun)
+                                    HStack {
+                                        Spacer()
+                                        Text("\(date[0]) \(date[1]) \(date[2])")
+                                            .foregroundStyle(.bovandakutyun)
+                                    }
+                                }
+                                .padding()
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .lineLimit(5)
+                                .background(color)
+                                .cornerRadius(15)
+                            })
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+            }
+        }
+        Button(action: {
+            showPopup = true
+        }, label: {
+            Image(systemName: "plus")
+                .resizable()
+                .frame(width: 30, height: 30)
+                .foregroundStyle(.white)
+        })
+        .frame(width: 50, height: 50)
+        .background(.red)
+        .cornerRadius(100)
+        .sheet(isPresented: $showPopup) {
+            Note(auth: authenticationManager, onNoteAdded: {
+                showPopup = false
+                dataManager.getNotes()
+            })
+        }
+        .sheet(isPresented: $showDetails) {
+            NoteDetails(noteDetails: $noteDetails, onNoteDeleted: {
+                showDetails = false
+                dataManager.getNotes()
+            })
         }
         .alert("Are you sure you want to log out?",isPresented: $logoutAlert) {
             Button(action: {
@@ -70,6 +150,7 @@ struct Profile: View {
             })
             Button("Cancel", role: .cancel) {}
         }
+        
         .alert("Something went wrong, try again!",isPresented: $errorAlert) {
             Button("Cancel", role: .cancel) {
                 errorAlert = false
